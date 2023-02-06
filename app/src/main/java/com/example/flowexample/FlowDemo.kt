ee7fe17c88216b1318953145ce26e941bdbc752e
raj.kayasth <raj.kayasth@brainvire.com>
2023-02-06 17:36:03 +0530

    - kotlin operations

--- app/src/main/java/com/example/flowexample/FlowDemo.kt
+++ app/src/main/java/com/example/flowexample/FlowDemo.kt
@@ -1,22 +1,38 @@
 package com.example.flowexample
 
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
 
 fun main() {
-    runBlocking {
+    /*runBlocking {
         createFlow().collect {
             println(it)
         }
-    }
+    }*/
+
+    /* runBlocking {
+         createFlowOf().collect {
+             println("CreateFlows $it")
+         }
+     }*/
+
+
+    /*runBlocking {
+        createFlowAS().collect {
+            println("As Flow $it")
+        }
+    }*/
+
 
     runBlocking {
-        createFlowOf().collect {
-            println("CreateFlows $it")
-        }
+        requestNumbers()
+            .filter { number -> number > 2 }
+            .map { number -> toUiModel(number) }
+            .catch { error -> println(error) }
+            .collect { println(it) }
     }
+
 }
 
 /**
@@ -33,3 +49,27 @@ fun createFlow() = flow<Int> {
  * */
 
 fun createFlowOf() = flowOf(1, 2, 3, 4, 5)
+
+
+/**
+ * asFlow() u can convert any collection like(List,Arraylist,etc) and sequence to flow using asFlow()
+ * */
+
+fun createFlowAS() = listOf<Int>(1, 2, 3, 4, 5).asFlow()
+
+/**
+ * Flow Operations
+ * @property = intermediate :- Intermediate operators are used to modifying the data flow between the producer and the consumer in order to meet the requirements of the following layer.
+ * @property = terminal
+ *
+ * @Links = https://medium.com/yemeksepeti-teknoloji/introduction-to-kotlin-flows-827f5a71ad7e
+ * */
+
+fun requestNumbers() = flow {
+    (1..5).forEach {
+        emit(it)
+        delay(100)
+    }
+}
+
+fun toUiModel(number: Int) = "Model $number"
