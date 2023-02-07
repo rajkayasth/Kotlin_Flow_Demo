edd771457c505c1a3028b62f5db5b161d7875d1c
raj.kayasth <raj.kayasth@brainvire.com>
2023-02-07 12:03:58 +0530

    - flow commands that are used normally

--- app/src/main/java/com/example/flowexample/MainActivity.kt
+++ app/src/main/java/com/example/flowexample/MainActivity.kt
@@ -3,32 +3,44 @@ package com.example.flowexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       /*val job =*/ GlobalScope.launch {
-            val data: Flow<Int> = producer()
-            data.collect {
-                Log.d("FLOWSDEMO - 1", it.toString())
-            }
+        /*val job =*/ GlobalScope.launch(Dispatchers.Main) {
+            producer()
+                .onStart {
+                    Log.d("FLOWSDEMO", "onCreate: Starting Outl")
+                }
+                .onCompletion {
+                    Log.d("FLOWSDEMO", "Completed")
+
+                }
+                .onEach {
+                    Log.d("FLOWSDEMO", "About to Emit $it")
+
+                }
+                .collect {
+                    Log.d("FLOWSDEMO", it.toString())
+                }
+
         }
         /*
         GlobalScope.launch {
-            *//**
-             * to cancel a flow if flow have no consumer to collect the flow it automatic cancel the flow
-             *
-             * @other that we can cancel the coroutine scope which user to collect flow
-             *
-             * in this method we have added delay of 3500 millis and after then job object cancel the scope
-             * *//*
+            */
+        /**
+         * to cancel a flow if flow have no consumer to collect the flow it automatic cancel the flow
+         *
+         * @other that we can cancel the coroutine scope which user to collect flow
+         *
+         * in this method we have added delay of 3500 millis and after then job object cancel the scope
+         * *//*
             delay(3500)
             job.cancel()
         }
@@ -39,13 +51,13 @@ class MainActivity : AppCompatActivity() {
          * create multiple consumer to consume the same flow
          * */
 
-        GlobalScope.launch {
+        /*GlobalScope.launch {
             val data: Flow<Int> = producer()
             delay(2500)
             data.collect {
                 Log.d("FLOWSDEMO - 2", it.toString())
             }
-        }
+        }*/
 
     }
 
