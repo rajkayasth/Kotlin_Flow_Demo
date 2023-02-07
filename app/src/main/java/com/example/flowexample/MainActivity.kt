48fe86c8e0745668ec678e3a40fc292ac57ee7a0
raj.kayasth <raj.kayasth@brainvire.com>
2023-02-07 11:34:53 +0530

    - multiple consumer to collect the same flow

--- app/src/main/java/com/example/flowexample/MainActivity.kt
+++ app/src/main/java/com/example/flowexample/MainActivity.kt
@@ -14,27 +14,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       val job = GlobalScope.launch {
+       /*val job =*/ GlobalScope.launch {
             val data: Flow<Int> = producer()
             data.collect {
-                Log.d("FLOWSDEMO", it.toString())
+                Log.d("FLOWSDEMO - 1", it.toString())
             }
         }
+        /*
         GlobalScope.launch {
-            /**
+            *//**
              * to cancel a flow if flow have no consumer to collect the flow it automatic cancel the flow
              *
              * @other that we can cancel the coroutine scope which user to collect flow
              *
              * in this method we have added delay of 3500 millis and after then job object cancel the scope
-             * */
+             * *//*
             delay(3500)
             job.cancel()
         }
+        */
+
+
+        /**
+         * create multiple consumer to consume the same flow
+         * */
+
+        GlobalScope.launch {
+            val data: Flow<Int> = producer()
+            delay(2500)
+            data.collect {
+                Log.d("FLOWSDEMO - 2", it.toString())
+            }
+        }
+
     }
 
     fun producer() = flow<Int> {
-        val list = listOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        val list = listOf<Int>(1, 2, 3, 4, 5/*, 6, 7, 8, 9, 10*/)
         list.forEach {
             delay(1000)
             emit(it)
