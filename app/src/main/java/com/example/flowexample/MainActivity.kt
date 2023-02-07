95229aaaf17723db9998b5b8a8fa9f5e8d9f813d
raj.kayasth <raj.kayasth@brainvire.com>
2023-02-07 11:12:24 +0530

    - flow  emit value using producer and consumer concept

--- app/src/main/java/com/example/flowexample/MainActivity.kt
+++ app/src/main/java/com/example/flowexample/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.flowexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        GlobalScope.launch {
+            val data: Flow<Int> = producer()
+            data.collect {
+                Log.d("FLOWSDEMO", it.toString())
+            }
+
+        }
+    }
+
+    fun producer() = flow<Int> {
+        val list = listOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        list.forEach {
+            delay(1000)
+            emit(it)
+        }
     }
 }
+
