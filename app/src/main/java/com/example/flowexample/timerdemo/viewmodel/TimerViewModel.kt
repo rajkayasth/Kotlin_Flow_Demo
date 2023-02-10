8a94bd7bee507bcefb5fb1299bf99f3e6fd45c4a
raj.kayasth <raj.kayasth@brainvire.com>
2023-02-10 15:55:54 +0530

    - state flow and shared flow example

--- app/src/main/java/com/example/flowexample/timerdemo/viewmodel/TimerViewModel.kt
+++ app/src/main/java/com/example/flowexample/timerdemo/viewmodel/TimerViewModel.kt
@@ -1,6 +1,47 @@
 package com.example.flowexample.timerdemo.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.flowexample.timerdemo.model.Post
+import com.example.flowexample.timerdemo.model.ProfileState
+import com.example.flowexample.timerdemo.model.User
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
 
-class TimerViewModel:ViewModel() {
+class TimerViewModel : ViewModel() {
+
+    private val isAutheticated = MutableStateFlow(true)
+    private val user = MutableStateFlow<User?>(null)
+    private val posts = MutableStateFlow(emptyList<Post>())
+
+    private val _profileState = MutableStateFlow<ProfileState?>(null)
+    val profileState = _profileState.asStateFlow()
+
+    private val flow1 = (1..10).asFlow().onEach { delay(1000) }
+    private val flow2 = (10..20).asFlow().onEach { delay(300) }
+    var numberString = MutableStateFlow("")
+        private set
+
+    init {
+        isAutheticated.combine(user) { isAutheticated, user ->
+            if (isAutheticated) user else null
+
+        }.combine(posts) { user, posts ->
+            user?.let {
+                _profileState.value = profileState.value?.copy(
+                    profilePicUrl = user.profilePicUrl,
+                    userName = user.userName,
+                    description = user.description,
+                    post = posts
+                )
+            }
+        }.launchIn(viewModelScope)
+
+        flow1.zip(flow2) { number1, number2 ->
+
+
+
+        }
+    }
+
 }
